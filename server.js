260d640b5d2cb8348859b2c6846a7cb966fd715a
Muhammad Shehroz <shehrozkhushi45@gmail.com>
2026-07-18 15:51:42 +0500

    Add about cars page

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
-const path = require('path');
 const PORT = process.env.PORT || 3000;
+
+// CSS, images aur JS public folder se serve honge
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.get("/about-cars.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "about-cars.html"));
 });
 
 app.listen(PORT, "0.0.0.0", () => {
